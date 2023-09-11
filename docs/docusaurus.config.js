465bc8f18e4ba34d680dfeaf940b097910f0d721
jgjfuture <jgjfuture@outlook.jp>
2023-09-11 02:30:43 +0000

    change index page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            to: 'protodocs/account/v1/addressbook.proto',
+            to: 'protodocs/sample/v1/address_book.proto',
             activeBasePath: 'protodocs',
             label: 'Protodocs',
             position: 'left',
