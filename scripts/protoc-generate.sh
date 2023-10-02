dacda05ebe40c0a6f3f51c897d93821b97acbbfa
jgjfuture <jgjfuture@outlook.jp>
2023-10-02 07:23:06 +0000

    Add Pushback Workflow

--- scripts/protoc-generate.sh
+++ scripts/protoc-generate.sh
@@ -12,3 +12,16 @@ find "$PROTO_PATH" -type f -iregex ".*.proto" -printf "%P\0" | \
     -v $(pwd)/docs:/out \
     -v $PROTO_PATH:/protos \
     pseudomuto/protoc-gen-doc --doc_opt=json,my-sample-scheme-docs.json
+
+# Generate go file from proto file
+# Using protoc-gen-go Docker image
+docker build -t protoc-gen-go docker/protoc-go
+find "$PROTO_PATH" -type f -iregex ".*.proto" -printf "%P\0" | \
+    xargs -0 \
+    docker run --rm \
+    -v "$(pwd):/repo_root" \
+    --workdir=/repo_root \
+    protoc-gen-go protoc \
+    --proto_path=./proto \
+    --go_out=plugins=grpc:go \
+    --go_opt=paths=source_relative
